ec5d8825e05201c1746df374b8f4f941f4fd6da6
Arman Sarkissian <sarkissarm@gmail.com>
2026-05-03 08:00:40 +0300

    Add components Categories ProductCard ProductList with types

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductCard = ({product}: {product:ProductType}) => {
+  return (
+    <div>
+        Card
+    </div>
+  );
+};
+
+export default ProductCard;
